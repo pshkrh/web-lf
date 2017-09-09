c355d22d7f2395965c45fd19c56a6efcf52bffed
Pushkar Kurhekar <pshkrh@gmail.com>
2017-09-10 00:50:42 +0530

    Temporarily removed Confirm Password Input

--- index.php
+++ index.php
@@ -162,9 +162,6 @@
                             <input type="password" class="form-control" placeholder="Password">
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Confirm Password">
-                        </div>
-                        <div class="form-group">
                             <input type="email" class="form-control" placeholder="Email">
                         </div>
                         <a href="#" data-toggle="modal" data-target="#popUpWindow" data-dismiss="modal">Already Registered?</a>
