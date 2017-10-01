c526cc839a296093f7cb10c19eeb0273141644e0
Pushkar Kurhekar <pshkrh@gmail.com>
2017-10-01 19:11:36 +0530

    Renamed found.html to found.php

---
+++ found.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Lost & Found | Home</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!--Custom CSS--->
+    <link rel="stylesheet" href="./css/style.css">
+
+  </head>
+  <body>
+
+    <?php include 'allpages.php';?>
+    This is in found
+    <?php include 'footer.php';?>
+    </body>
+    </html>
