e3fa05a1e4949ccdefb90a3d7e5e672fb80d4870
Pushkar Kurhekar <pshkrh@gmail.com>
2017-10-11 01:24:00 +0530

    Add claim.php

---
+++ claim.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Lost & Found | Home</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+
+    <!--Custom CSS--->
+    <link rel="stylesheet" href="./css/style2.css">
+
+  </head>
+  <body>
+  <?php
+  include 'loginpages.php';
+  include 'includes/dbh.php';
+  ?>
+
+  <div class="container">
+    <h1 class="display-1">Claims</h1>
+    <h6 class="text-muted display-4">You can view all the lost items that have been reported here.</h6>
+    <hr>
+  </div>
+
+  <?php
+  $sql = "SELECT id,type,model,location,status,user FROM lost";
+  $result=mysqli_query($conn,$sql);
+
+  echo"<div class='container'>";
+  echo "<table class='table table-striped table-hover'>
+  <thead class='thead-inverse'>
+  <tr>
+  <th>ID</th>
+  <th>Type</th>
+  <th>Model</th>
+  <th>Location</th>
+  <th>Status</th>
+  <th>Reported By</th>
+  </tr>
+  </thead>";
+  while($row = mysqli_fetch_array($result))
+  {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['type'] . "</td>";
+    echo "<td>" . $row['model'] . "</td>";
+    echo "<td>" . $row['location'] . "</td>";
+    echo "<td>" . $row['status'] . "</td>";
+    echo "<td>" . $row['user'] . "</td>";
+    echo "</tr>";
+  }
+  echo "</table>";
+  echo "</div>";
+
+mysqli_close($conn);
+
+  include 'footer.php';?>
+    </body>
+    </html>
