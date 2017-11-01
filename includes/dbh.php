e8429358c4e84b1502075f45bd6c0289c298baa3
Pushkar Kurhekar <pshkrh@gmail.com>
2017-11-02 01:40:56 +0530

    Fixed typo

--- includes/dbh.php
+++ includes/dbh.php
@@ -3,7 +3,7 @@
 $conn= mysqli_connect("localhost","root","","login");
 if(!$conn)
 {
-  die("Connection failed: ".msqli_connect_error());
+  die("Connection failed: ".mysqli_connect_error());
 }
 
 ?>
